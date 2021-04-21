36b347b6e52a93c2f9143b2fe9a1c67ab1832102
LisachGit <82966745+LisachGit@users.noreply.github.com>
2021-04-21 20:37:51 +0300

    Добавьте файлы проекта.

---
+++ Kursach 2.0/WelcomeForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Kursach_2._0
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AboutOnWelcomeForm = new System.Windows.Forms.Label();
+            this.ExitIcon = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.StartMainMenuButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(298, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Устный счёт";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(482, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Подготовил студент группы 240418 Лисицкий Павел";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(576, 48);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Под руководством  доцента кафедры прикладной математики\r\nинформатики математическ" +
+    "ого факультета БГПУ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(10, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 33);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Шербаф А.И.";
+            // 
+            // AboutOnWelcomeForm
+            // 
+            this.AboutOnWelcomeForm.AutoSize = true;
+            this.AboutOnWelcomeForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AboutOnWelcomeForm.Location = new System.Drawing.Point(744, 9);
+            this.AboutOnWelcomeForm.Name = "AboutOnWelcomeForm";
+            this.AboutOnWelcomeForm.Size = new System.Drawing.Size(18, 20);
+            this.AboutOnWelcomeForm.TabIndex = 8;
+            this.AboutOnWelcomeForm.Text = "?";
+            this.AboutOnWelcomeForm.Click += new System.EventHandler(this.AboutOnWelcomeForm_Click);
+            this.AboutOnWelcomeForm.MouseLeave += new System.EventHandler(this.AboutOnWelcomeForm_MouseLeave);
+            this.AboutOnWelcomeForm.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AboutOnWelcomeForm_MouseMove);
+            // 
+            // ExitIcon
+            // 
+            this.ExitIcon.AutoSize = true;
+            this.ExitIcon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitIcon.Location = new System.Drawing.Point(768, 9);
+            this.ExitIcon.Name = "ExitIcon";
+            this.ExitIcon.Size = new System.Drawing.Size(20, 20);
+            this.ExitIcon.TabIndex = 9;
+            this.ExitIcon.Text = "X";
+            this.ExitIcon.Click += new System.EventHandler(this.ExitIcon_Click);
+            this.ExitIcon.MouseLeave += new System.EventHandler(this.ExitIcon_MouseLeave);
+            this.ExitIcon.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ExitIcon_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Kursach_2._0.Properties.Resources._143946_tekstura_drevesiny_lak_laminat_morilka_tekstura_dereva_500x;
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 378);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(802, 72);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Kursach_2._0.Properties.Resources._143946_tekstura_drevesiny_lak_laminat_morilka_tekstura_dereva_500x;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Kursach_2._0.Properties.Resources.depositphotos_318889202_stock_photo_green_blackboard_or_chalkboard;
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 60);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(802, 320);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseDown);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseMove);
+            // 
+            // StartMainMenuButton
+            // 
+            this.StartMainMenuButton.BackColor = System.Drawing.Color.Transparent;
+            this.StartMainMenuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartMainMenuButton.Location = new System.Drawing.Point(286, 386);
+            this.StartMainMenuButton.Name = "StartMainMenuButton";
+            this.StartMainMenuButton.Size = new System.Drawing.Size(219, 65);
+            this.StartMainMenuButton.TabIndex = 13;
+            this.StartMainMenuButton.Text = "Запустить";
+            this.StartMainMenuButton.UseVisualStyleBackColor = false;
+            this.StartMainMenuButton.Click += new System.EventHandler(this.StartMainMenuButton_Click);
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StartMainMenuButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.ExitIcon);
+            this.Controls.Add(this.AboutOnWelcomeForm);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WelcomeForm";
+            this.Text = "WelcomeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label AboutOnWelcomeForm;
+        private System.Windows.Forms.Label ExitIcon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button StartMainMenuButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Kursach 2.0/WelcomeForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursach_2._0
+{
+    public partial class WelcomeForm : Form
+    {
+
+        AboutForm aboutFormOnWelcomeForm = new AboutForm();
+        MainForm mainFormOnWelcomeForm = new MainForm();
+        Point lastPoint;
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitIcon_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ExitIcon_MouseMove(object sender, MouseEventArgs e)
+        {
+            ExitIcon.ForeColor = Color.Red;
+        }
+
+        private void ExitIcon_MouseLeave(object sender, EventArgs e)
+        {
+            ExitIcon.ForeColor = Color.Black;
+        }
+
+        private void StartMainMenuButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            mainFormOnWelcomeForm.Show();
+        }
+
+        private void AboutOnWelcomeForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            AboutOnWelcomeForm.ForeColor = Color.Cyan;
+            toolTip1.SetToolTip(AboutOnWelcomeForm, "О программе");
+        }
+
+        private void AboutOnWelcomeForm_MouseLeave(object sender, EventArgs e)
+        {
+            AboutOnWelcomeForm.ForeColor = Color.Black;
+        }
+
+        private void AboutOnWelcomeForm_Click(object sender, EventArgs e)
+        {
+            aboutFormOnWelcomeForm.Show();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
